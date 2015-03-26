ce9298c6504b6a1cc0038cd839e426b7e0b51a6f
Miro Svrtan <miro@mirosvrtan.me>
2015-03-26 21:57:14 +0100

    Updated tests for using Sf DIC #35

--- src/NullDev/Nemesis/Tests/Unit/Command/GenerateTestsCommandTest.php
+++ src/NullDev/Nemesis/Tests/Unit/Command/GenerateTestsCommandTest.php
@@ -24,24 +24,30 @@ class GenerateTestsCommandTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecute()
     {
+        $mockPackageSettings = m::mock('NullDev\Nemesis\Settings\PackageSettings');
+        $mockPackageSettings
+            ->shouldReceive('setPath')->with('/src/')->once();
+        $mockPackageSettings
+            ->shouldReceive('setType')->with('sf2')->once();
+        $mockPackageSettings
+            ->shouldReceive('addExcludeFolders')->with('Tests')->once();
+
+        $mockContainer = m::mock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $mockContainer
+            ->shouldReceive('get')->with('settings')->once()->andReturn($mockPackageSettings);
+
         $command = new GenerateTestsCommand('name');
+        $command->setContainer($mockContainer);
 
         $mockInput = m::mock('Symfony\Component\Console\Input\InputInterface');
         $mockInput
-            ->shouldReceive('getArgument')
-            ->with('path')
-            ->once()
-            ->andReturn('/src/');
+            ->shouldReceive('getArgument')->with('path')->once()->andReturn('/src/');
         $mockInput
-            ->shouldReceive('getArgument')
-            ->with('type')
-            ->once()
-            ->andReturn('sf2');
+            ->shouldReceive('getArgument')->with('type')->once()->andReturn('sf2');
+
         $mockOutput = m::mock('Symfony\Component\Console\Output\OutputInterface');
         $mockOutput
-            ->shouldReceive('writeln')
-            ->with('/src/')
-            ->once();
+            ->shouldReceive('writeln')->with('/src/')->once();
 
         $result = $command->execute($mockInput, $mockOutput);
 
