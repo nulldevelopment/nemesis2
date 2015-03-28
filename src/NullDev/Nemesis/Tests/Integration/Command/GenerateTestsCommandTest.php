cf8a6da9726579a3d60a0683f77a19a7fa8e6f0e
Miro Svrtan <miro@mirosvrtan.me>
2015-03-28 16:58:44 +0100

    Fiddling with SourceMetaDataCollectionGeneratorTest

--- src/NullDev/Nemesis/Tests/Integration/Command/GenerateTestsCommandTest.php
+++ src/NullDev/Nemesis/Tests/Integration/Command/GenerateTestsCommandTest.php
@@ -2,12 +2,18 @@
 namespace NullDev\Nemesis\Tests\Integration\Command;
 
 use NullDev\Nemesis\Command\GenerateTestsCommand;
+use NullDev\Nemesis\Tests\Integration\ContainerTrait;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 /**
  *
  */
 class GenerateTestsCommandTest extends \PHPUnit_Framework_TestCase
 {
+    use ContainerTrait;
+
     /**
      * @var GenerateTestsCommand
      */
@@ -20,6 +26,8 @@ class GenerateTestsCommandTest extends \PHPUnit_Framework_TestCase
         $name = 'name';
 
         $this->object = new GenerateTestsCommand($name);
+
+        $this->object->setContainer($this->getContainer());
     }
 
     /**
@@ -27,6 +35,12 @@ class GenerateTestsCommandTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecute()
     {
-        $this->markTestIncomplete('TODO');
+        $this->markTestSkipped('TODO:not sure how to prcosee input argument without building it from scratch :(');
+        $inputDef = new InputDefinition(['path', 'type']);
+
+        $input  = new ArrayInput(['path' => NEMESIS_TESTDATA_PATH, 'type' => 'sf2'], $inputDef);
+        $output = new ConsoleOutput();
+
+        $this->object->execute($input, $output);
     }
 }
