7e878637cae56f2f3c8946fc3bd094b360af102b
Miro Svrtan <miro@mirosvrtan.me>
2015-03-28 16:56:43 +0100

    Upgrade PackageSettings with namespaces and root test path #43

--- src/NullDev/Nemesis/Settings/PackageSettings.php
+++ src/NullDev/Nemesis/Settings/PackageSettings.php
@@ -2,6 +2,8 @@
 
 namespace NullDev\Nemesis\Settings;
 
+use Exception;
+
 /**
  * Class PackageSettings.
  */
@@ -9,14 +11,23 @@ class PackageSettings
 {
     protected $path;
     protected $type;
+    protected $rootNamespace;
+    protected $rootTestPath;
+    protected $rootTestNamespace;
 
     protected $excludeFolders = [];
 
     /**
      * @return string
+     *
+     * @throws Exception
      */
     public function getPath()
     {
+        if ($this->path === null) {
+            throw new Exception('Root package path is not defined!');
+        }
+
         return $this->path;
     }
 
@@ -42,6 +53,66 @@ class PackageSettings
     public function setType($type)
     {
         $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRootNamespace()
+    {
+        if (null === $this->rootNamespace) {
+            return $this->calculateRootNamespaceFromRootPath($this->getPath());
+        }
+
+        return $this->rootNamespace;
+    }
+
+    /**
+     * @param mixed $rootNamespace
+     */
+    public function setRootNamespace($rootNamespace)
+    {
+        $this->rootNamespace = $rootNamespace;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRootTestPath()
+    {
+        if (null === $this->rootTestPath) {
+            return $this->getPath().'/Tests';
+        }
+
+        return $this->rootTestPath;
+    }
+
+    /**
+     * @param mixed $rootTestPath
+     */
+    public function setRootTestPath($rootTestPath)
+    {
+        $this->rootTestPath = $rootTestPath;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRootTestNamespace()
+    {
+        if (null === $this->rootTestNamespace) {
+            return $this->getRootNamespace().'\Tests';
+        }
+
+        return $this->rootTestNamespace;
+    }
+
+    /**
+     * @param mixed $rootTestNamespace
+     */
+    public function setRootTestNamespace($rootTestNamespace)
+    {
+        $this->rootTestNamespace = $rootTestNamespace;
     }
 
     /**
@@ -74,5 +145,32 @@ class PackageSettings
         $this->excludeFolders[] = $excludeFolder;
 
         return $this;
+    }
+
+    /**
+     * @param string $rootPath
+     *
+     * @return mixed
+     *
+     * @throws \Exception
+     */
+    public function calculateRootNamespaceFromRootPath($rootPath)
+    {
+        if (substr($rootPath, -1) === '/') {
+            $rootPath = substr($rootPath, 0, -1);
+        }
+
+        //Locates last occurrence of 'src/' in given path.
+        $regex = '/.*src\/(?<namespace>.*)$/';
+
+        //Regex occurrence hasn't matched 'src/' in path.
+        if (!preg_match($regex, $rootPath, $matches)) {
+            throw new \Exception('Root namespace not recognizable in '.$rootPath);
+        }
+
+        //Replace '/' with namespace separator.
+        $namespace = str_replace('/', '\\', $matches['namespace']);
+
+        return $namespace;
     }
 }
